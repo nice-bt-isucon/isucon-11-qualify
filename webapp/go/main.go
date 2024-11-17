21de38e2557839ebeafb403fad8291c342761f50
kakeru-one <ochisyoji1118@gmail.com>
2024-11-17 17:41:06 +0900

    POST /api/condition/:jia_isu_uuidでbulk insertする

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1195,6 +1195,28 @@ func postIsuCondition(c echo.Context) error {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
+	// for _, cond := range req {
+	// 	timestamp := time.Unix(cond.Timestamp, 0)
+
+	// 	if !isValidConditionFormat(cond.Condition) {
+	// 		return c.String(http.StatusBadRequest, "bad request body")
+	// 	}
+
+	// 	_, err = tx.Exec(
+	// 		"INSERT INTO `isu_condition`"+
+	// 			"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
+	// 			"	VALUES (?, ?, ?, ?, ?)",
+	// 		jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
+	// 	if err != nil {
+	// 		c.Logger().Errorf("db error: %v", err)
+	// 		return c.NoContent(http.StatusInternalServerError)
+	// 	}
+
+	// }
+	query := "INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES "
+	values := []interface{}{}
+	placeholders := []string{}
+
 	for _, cond := range req {
 		timestamp := time.Unix(cond.Timestamp, 0)
 
@@ -1202,16 +1224,15 @@ func postIsuCondition(c echo.Context) error {
 			return c.String(http.StatusBadRequest, "bad request body")
 		}
 
-		_, err = tx.Exec(
-			"INSERT INTO `isu_condition`"+
-				"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
-				"	VALUES (?, ?, ?, ?, ?)",
-			jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
-		}
+		placeholders = append(placeholders, "(?, ?, ?, ?, ?)")
+		values = append(values, jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
+	}
 
+	query += strings.Join(placeholders, ",")
+	_, err = tx.Exec(query, values...)
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	err = tx.Commit()
