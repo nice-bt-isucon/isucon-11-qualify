2b8dd12afbf40a3a8667290b4dd9cac70c5da8bc
kakeru-one <ochisyoji1118@gmail.com>
2024-11-17 18:57:59 +0900

    dropProbabilityを復活させる

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math/rand/v2"
 	"net"
 	"net/http"
 	"os"
@@ -1187,6 +1188,13 @@ func getTrend(c echo.Context) error {
 // POST /api/condition/:jia_isu_uuid
 // ISUからのコンディションを受け取る
 func postIsuCondition(c echo.Context) error {
+	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
+	dropProbability := 0.9
+	if rand.Float64() <= dropProbability {
+		c.Logger().Warnf("drop post isu condition request")
+		return c.NoContent(http.StatusAccepted)
+	}
+
 	jiaIsuUUID := c.Param("jia_isu_uuid")
 	if jiaIsuUUID == "" {
 		return c.String(http.StatusBadRequest, "missing: jia_isu_uuid")
