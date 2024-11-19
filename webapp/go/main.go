3ba05269ba1cf38cba5f1b96e0e2c42af0134003
fizzkazz <nk.343586p@gmail.com>
2024-11-19 19:46:28 +0900

    pprotain 組み込み

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	"github.com/kaz/pprotein/integration/echov4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -222,6 +223,8 @@ func main() {
 	e := echo.New()
 	e.Debug = true
 	e.Logger.SetLevel(log.DEBUG)
+
+	echov4.EnableDebugHandler(e)
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
